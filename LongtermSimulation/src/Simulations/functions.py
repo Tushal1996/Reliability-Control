383ab3f1f239561822d64a977a15f857d9db58ad
Tushal1996 <tushalpatel1996@gmail.com>
2024-10-09 20:24:29 +0200

    Update functions.py

--- LongtermSimulation/src/Simulations/functions.py
+++ LongtermSimulation/src/Simulations/functions.py
@@ -5,12 +5,11 @@
 '''
 Created on 01.09.1009
 
-@author: reqnik
-'''
+
 import numpy as np
 
 class B_transformation():
-    # @reqnik: Explaination? 
+
     def __init__(self, parameters):
         self._parameters=parameters
     
@@ -24,7 +23,6 @@ class B_transformation():
     
     def calcSTD(self,windspeed,B_value):
         TI = (self._parameters[0] + windspeed * self._parameters[1] + self._parameters[2] * B_value + self._parameters[3] * windspeed * B_value)
-        # @reqnik: function name says calcSTD but the return variable is TI?
         return TI
     
     def calcBFromSTD(self,windspeed,stdVal):
@@ -34,7 +32,6 @@ class B_transformation():
 
 
 def get_interpolation(ranges, a, p):
-    # @reqnik: is this a general interpolation function? What is a, what is p?
     for i in range(a.ndim):
         # check if we are out of range, if yes than use the last bounded values
         if p[i] <= ranges[i][0]:
